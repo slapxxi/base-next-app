7f34ae49e67729dd6eb97dfb81cb130f29c50d0c
Slava Pavlutin <sl.pavlutin@gmail.com>
2020-11-11 14:15:29 +0300

    add env and types

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,19 @@
+import { Db } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { defaultTheme } from './style/theme';
 
 export type Theme = typeof defaultTheme;
+
+export interface MiddlewareRequest extends NextApiRequest {
+  db?: Db;
+}
+
+export type DbApiHandler = (req: MiddlewareRequest, res: NextApiResponse) => void;
+
+export type APIResponse<T = undefined> =
+  | (T extends undefined ? { status: 'ok' } : { status: 'ok'; data: T })
+  | { status: 'error'; message: string };
+
+export type Serialized<T> = {
+  [P in keyof T]: T[P] extends Date ? string : T[P];
+};
